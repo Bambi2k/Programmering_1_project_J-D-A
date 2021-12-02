2eb7dad5383a89dd75294c1fb3a4c189bab1ba0b
Alexander Holmgren <alexander.holmgren@icloud.com>
2021-12-02 12:35:59 +0100

    Co-authored-by: Jamames <Jamames@users.noreply.github.com>

--- playing_in_the_dark.py
+++ playing_in_the_dark.py
@@ -6,12 +6,13 @@ import random
 import multiprocessing
 import threading
 from pygame import mixer
+import text_functions
 
 
 # GLÖMM INTE ÄNDRA PLANERINGEN OM DU ÄNDRAR KODEN!!!
 
 # De tre olika scenarios som kan förekomma när man öppnar dörrarna
-scenarios = ["Monster", "Monster", "Monster", "Trap", "Treasure", "Treasure"]
+SCENARIOS = ["Monster", "Monster", "Monster", "Trap", "Treasure", "Treasure"]
 
 
 # Klassen "player" som huvudkaraktären kommer att få
@@ -40,41 +41,13 @@ def intro_music():
 intro = threading.Thread(target=intro_music, args=())
 
 
-# Bokstav för bokstav print, för en mer långsam och förstårbar upplevelse
-
-def print_slow(str):
-    for letter in str:
-        sys.stdout.write(letter)
-        sys.stdout.flush()
-        time.sleep(0.0)
-        # times.sleep(0.05)
-
-# Bokstav för bokstav print men något snabbare än den ovan
-
-
-def print_medium(str):
-    for letter in str:
-        sys.stdout.write(letter)
-        sys.stdout.flush()
-        # time.sleep(0.03)
-        time.sleep(0)
-
-# Super långsam print grej
-
-
-def print_superslow(str):
-    for letter in str:
-        sys.stdout.write(letter)
-        sys.stdout.flush()
-
-        time.sleep(0.7)
 # Funktionen som kommer spelas när du väljer att titta igenom ditt inventory(bag)
 
 
 def bagcheck(player):
     print("\n")
     bag_list = []
-    print_slow("Your inventory: ")
+    text_functions.print_slow("Your inventory: ")
     for item in player.BAG:
         bag_list.append(item.NAME)
     print(bag_list)
@@ -85,83 +58,75 @@ def bagcheck(player):
 
 
 def statcheck(player):
-    print_slow("Your stats in order of HP,STR,LVL")
+    text_functions.print_slow("Your stats in order of HP,STR,LVL")
     print("\n")
-    print_slow(str(player.HP))
+    text_functions.print_slow(str(player.HP))
     print("\n")
-    print_slow(str(player.STR))
+    text_functions.print_slow(str(player.STR))
     print("\n")
-    print_slow(str(player.LVL))
+    text_functions.print_slow(str(player.LVL))
     print("\n")
 
 
 def monsterfight(player):
     monsterstr = random.randint(2, 12)
     print("\n")
-    print_medium("You have encountered a monster! ")
-    print_medium("It has a strenghth of: ")
-    print_medium(str(monsterstr))
+    text_functions.print_slow("You have encountered a monster! ")
+    text_functions.print_slow("It has a strenghth of: ")
+    text_functions.print_slow(str(monsterstr))
     print("\n")
     time.sleep(2)
     if monsterstr < player.STR:
-        print_medium("You won the battle and gained 1 LVL!")
+        text_functions.print_slow("You won the battle and gained 1 LVL!")
         player.LVL = player.LVL + 1
         print("\n")
     elif monsterstr == player.STR:
-        print_medium(
+        text_functions.print_slow(
             "The monster and you where evenly matched, and you managed to flee the monster unscathed.")
     else:
-        print_medium("You lost the battle and lost 1 HP")
+        text_functions.print_slow("You lost the battle and lost 1 HP")
         player.HP = player.HP - 1
         print("\n")
     time.sleep(2)
 
 
 def open_chest(player):
-
-    Broadsword = Item("Broadsword", 3)
-    Machete = Item("Machete", 2)
-    Katana = Item("Katana", 2)
-    Blade = Item("Blade", 2)
-    Knife = Item("Knife", 1)
-    Dagger = Item("Dagger", 1)
-    Shortsword = Item("Shortsword", 1)
-    Excalibur = Item("Excalibur", 5)
-    item_list = [Blade, Dagger, Shortsword,
-                 Excalibur, Knife, Katana, Machete, Broadsword]
+    item_list = [Item("Blade", 2), Item("Dagger", 1), Item("Shortsword", 1),
+                 Item("Excalibur", 5), Item("Knife", 1), Item("Katana", 2), Item("Machete", 2), Item("Broadsword", 3)]
     prize = random.choice(item_list)
     if len(player.BAG) < 5:
         player.BAG.append(prize)
-        print_medium("You have recieved an item: ")
+        text_functions.print_slow("You have recieved an item: ")
         print("\n")
-        print_medium(prize.NAME)
+        text_functions.print_slow(prize.NAME)
     else:
         print("\n")
-        print_medium("The chest contained the item: ")
+        text_functions.print_slow("The chest contained the item: ")
         print("\n")
-        print_medium(prize.NAME)
+        text_functions.print_slow(prize.NAME)
         print("\n")
-        print_slow(
+        text_functions.print_slow(
             "Your inventory is full! Would you like to swap an item?: (Yes/No) ")
         choice = input().lower().strip()
         if choice == "yes":
             bagcheck(player)
-            print_slow(
+            text_functions.print_slow(
                 "Select an item index (0-4) that you would like to swap: ")
             index = int(input().strip())
             player.BAG.pop(index)
             player.BAG.append(prize)
             print("\n")
-            print_medium("Your new item has been added to your inventory")
+            text_functions.print_slow(
+                "Your new item has been added to your inventory")
         if choice == "no":
-            print_slow("Alright then...")
+            text_functions.print_slow("Alright then...")
 
 
 def trap(player):
     damage = random.randint(1, 2)
     print("\n")
-    print_medium("You fell into a trap, and took damage: ")
-    print_slow(str(damage))
+    text_functions.print_slow("You fell into a trap, and took damage: ")
+    text_functions.print_slow(str(damage))
     player.HP = player.HP - damage
     print("\n")
 
@@ -176,7 +141,7 @@ def clear():
 
 def again1():
     time.sleep(1)
-    print_slow("Please enter a valid answer")
+    text_functions.print_slow("Please enter a valid answer")
     clear()
     start()
 
@@ -196,104 +161,25 @@ def start():
     intro_music()
     mixer.music.play(-1)
     player = Player(10, 4, 1, [])
-    print_slow("Would you like to start the game? (Yes/No): ")
+    text_functions.print_slow("Would you like to start the game? (Yes/No): ")
     play = input().lower().strip()
     if play == "yes":
 
-        print_slow("Would you like a tutorial? (Yes/No): ")
+        text_functions.print_slow("Would you like a tutorial? (Yes/No): ")
         want_tutorial = input().lower().strip()
         if want_tutorial == "yes":
-            tutorial()
-            intro()
+            text_functions.tutorial()
+            text_functions.intro()
             playloop(player)
         elif want_tutorial == "no":
-            intro()
+            text_functions.intro()
             playloop(player)
         elif play != "yes" or "no":
             again1()
     elif play == "no":
-        print_slow("Maybe next time")
+        text_functions.print_slow("Maybe next time")
     elif play != "yes" or "no":
         again1()
-
-
-def outro():
-    print_medium("Thank you for playing our game")
-    print("\n")
-    print_medium(
-        "Coded by (in no specific order): Alexander Holmgren, Johan James and David Salomonsson")
-    print("\n")
-    print_medium("Main director: Alexander Holmgren")
-    print("\n")
-    print_medium("Executive producer: Johan James")
-    print("\n")
-    print_medium("Music design and story writer: David Salomonsson")
-    print("\n")
-    print_medium("Outside help: Martin Loman")
-    print("\n")
-    print_medium("Thank you again for playing. Hope you enjoyed it :)")
-    print("\n")
-    print_medium("Lots of love from BCAA STUDIOS")
-
-
-# Förklaring till hur spelet fungerar och spelas
-
-
-def tutorial():
-    print_medium("Welcome to our game! It is quite simple. You will follow a charachter that will explore multiple rooms, collecting items and treasure, fighting monsters and (hopefully not) dealing with traps")
-    print("\n")
-    time.sleep(1)
-    print_medium("In each room you will be able to chose which action you want to execute. To open one of the doors: type open. To check stats: type stats. And if you want to check your bag: type bag")
-    print("\n")
-    time.sleep(1)
-    print_medium(
-        "The goal of the game is to get your LVL from 1 to 10, without dropping you HP below")
-    print("\n")
-    time.sleep(1)
-    print_medium("A trap room will deal a random amount of damage to your HP capped at 3. The treasure rooms will have a item that you can bring along to boost your strength. You can carry 5 items at a time. Lastly the monster room, where you will fight a monster with a random strength. If you have more strength than the monster, you win and gain 1 LVL while if you have less strength than the monster, you lose and forfeit 1 HP  ")
-    print("\n")
-    time.sleep(1)
-    print_medium("Pretty simple huh? Lets get this going shall we?")
-    print("\n")
-    time.sleep(2)
-    print("\n\n")
-
-
-# Introducerar spelets plats för att skapa atmosfär (mellan varje rad text printas en blank rad, och sedan är det en 1 sekunds paus innan nästa rad)
-
-def intro():
-    print("\n")
-    print_slow("Darkness")
-    # time.sleep(1)
-    print("\n")
-    print_slow("Nothing but darkness")
-    # time.sleep(1)
-    print("\n")
-    print_slow(
-        "You feel around you with your hands, struggling to understand what is going on")
-    # time.sleep(1)
-    print("\n")
-    print_slow("The ground is cold to the touch")
-    # time.sleep(1)
-    print("\n")
-    print_slow("You stand up and reach for your phone")
-    # time.sleep(1)
-    print("\n")
-    print_slow("No cell connection...")
-    # time.sleep(1)
-    print("\n")
-    print_slow("You turn on the flashlight and look around")
-    # time.sleep(1)
-    print("\n")
-    print_slow("I gotta get out of here")
-    # time.sleep(1)
-    print("\n")
-    # time.sleep(2)
-    print_slow("Walking a couple of meters reveales 3 identical doors.")
-    print("\n")
-    time.sleep(1)
-    mixer.music.fadeout(6000)
-    time.sleep(6)
 
 
 # Spelets loop som kör igenom spelet.
@@ -302,7 +188,7 @@ def playloop(player: Player):
     while player.HP > 0 and player.LVL < 10:
         total_str(player)
         print("\n")
-        print_slow(
+        text_functions.print_slow(
             "Would you like to: open a door(open), check stats(stats) or check inventory(bag)? ")
         choice = input().lower().strip()
         if choice == "bag":
@@ -310,14 +196,15 @@ def playloop(player: Player):
         if choice == "stats":
             statcheck(player)
         if choice == "open":
-            print_slow(
+            text_functions.print_slow(
                 "Which door would you like to choose? Door 1, 2 or 3? ")
             door_choice = input().lower().strip()
             if door_choice in ["1", "2", "3"]:
-                scen = random.choice(scenarios)
+                scen = random.choice(SCENARIOS)
             else:
                 print("\n")
-                print("Choose a door between 1 and 3 (you idiot)")
+                text_functions.print_slow(
+                    "Choose a door between 1 and 3 (you idiot)")
                 playloop(player)
             if scen == "Monster":
                 monsterfight(player)
@@ -330,64 +217,13 @@ def playloop(player: Player):
 
 def win_lose(player):
     if player.HP == 0:
-        print_medium("You died and lost the game. Better luck next time!")
+        text_functions.print_slow(
+            "You died and lost the game. Better luck next time!")
         print("\n")
         print("\n")
         start()
     elif player.LVL == 10:
-        print_medium(
-            "You reached lvl 10!")
-        time.sleep(1)
-        print("\n")
-        print_medium("You look around in the darkness")
-        time.sleep(1)
-        print("\n")
-        print_medium(
-            "Silhouttes of the monsters you have slain laying on the ice cold floor")
-        time.sleep(1)
-        print("\n")
-        print_medium("You hear footseps approaching your room.")
-        time.sleep(1)
-        print("\n")
-        print_medium(
-            "You hear the wooden door creak as light floods your room from the outside")
-        time.sleep(1)
-        print("\n")
-        print_medium(
-            "A beam of light, bright as the sun flickers to life above you")
-        time.sleep(1)
-        print("\n")
-        print_medium(
-            "'George?! What are you doing up so late?' you hear a familiar voice say")
-        time.sleep(1)
-        print("\n")
-        print_medium(
-            "'Uhhh... Nothing mom. Just cleaning my room' you respond instinctively")
-        time.sleep(1)
-        print("\n")
-        print_medium(
-            "'Well you did not do a very good job then. Go to bed NOW!' she said strictly")
-        time.sleep(1)
-        print("\n")
-        print_medium("'Alright mom' you answer.")
-        time.sleep(1)
-        print("\n")
-        print_medium(
-            "As you stand up and look around, you see your monster stuffed animals laying on the ground. Next to you is a foam sword")
-        time.sleep(1)
-        print("\n")
-        print_medium("'I wish I could keep playing' I said to myself")
-        time.sleep(1)
-        print("\n")
-        print_medium("The light turns of as you curl up in your bed")
-        time.sleep(1)
-        print("\n")
-        print_superslow("THE END")
-        print("\n")
-        print("\n")
-        time.sleep(6)
-        outro()
-        # endgame() Vi utvecklar senare
+        text_functions.win_story()
 
 
 clear()
---
+++ text_functions.py
@@ -0,0 +1,144 @@
+import time
+import sys
+import random as rand
+import random
+import multiprocessing
+import threading
+from pygame import mixer
+import text_functions
+
+
+def outro():
+    print_slow("Thank you for playing our game")
+    print("\n")
+    print_slow(
+        "Coded by (in no specific order): Alexander Holmgren, Johan James and David Salomonsson")
+    print("\n")
+    print_slow("Main director: Alexander Holmgren")
+    print("\n")
+    print_slow("Executive producer: Johan James")
+    print("\n")
+    print_slow("Music design and story writer: David Salomonsson")
+    print("\n")
+    print_slow("Outside help: Martin Loman")
+    print("\n")
+    print_slow("Thank you again for playing. Hope you enjoyed it :)")
+    print("\n")
+    print_slow("Lots of love from BCAA STUDIOS")
+
+
+def tutorial():
+    print_slow("Welcome to our game! It is quite simple. You will follow a charachter that will explore multiple rooms, collecting items and treasure, fighting monsters and (hopefully not) dealing with traps")
+    print("\n")
+    time.sleep(1)
+    print_slow("In each room you will be able to chose which action you want to execute. To open one of the doors: type open. To check stats: type stats. And if you want to check your bag: type bag")
+    print("\n")
+    time.sleep(1)
+    print_slow(
+        "The goal of the game is to get your LVL from 1 to 10, without dropping you HP below")
+    print("\n")
+    time.sleep(1)
+    print_slow("A trap room will deal a random amount of damage to your HP capped at 3. The treasure rooms will have a item that you can bring along to boost your strength. You can carry 5 items at a time. Lastly the monster room, where you will fight a monster with a random strength. If you have more strength than the monster, you win and gain 1 LVL while if you have less strength than the monster, you lose and forfeit 1 HP  ")
+    print("\n")
+    time.sleep(1)
+    print_slow("Pretty simple huh? Lets get this going shall we?")
+    print("\n")
+    time.sleep(2)
+    print("\n\n")
+
+
+def intro():
+    print("\n")
+    print_slow("Darkness")
+    # time.sleep(1)
+    print("\n")
+    print_slow("Nothing but darkness")
+    # time.sleep(1)
+    print("\n")
+    print_slow(
+        "You feel around you with your hands, struggling to understand what is going on")
+    # time.sleep(1)
+    print("\n")
+    print_slow("The ground is cold to the touch")
+    # time.sleep(1)
+    print("\n")
+    print_slow("You stand up and reach for your phone")
+    # time.sleep(1)
+    print("\n")
+    print_slow("No cell connection...")
+    # time.sleep(1)
+    print("\n")
+    print_slow("You turn on the flashlight and look around")
+    # time.sleep(1)
+    print("\n")
+    print_slow("I gotta get out of here")
+    # time.sleep(1)
+    print("\n")
+    # time.sleep(2)
+    print_slow("Walking a couple of meters reveales 3 identical doors.")
+    print("\n")
+    time.sleep(1)
+    mixer.music.fadeout(6000)
+    time.sleep(6)
+
+
+def print_slow(str):
+    for letter in str:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(0.0)
+        # times.sleep(0.05)
+
+# Bokstav för bokstav print men något snabbare än den ovan
+
+
+def win_story():
+    print_slow("You reached lvl 10!")
+    time.sleep(1)
+    print("\n")
+    print_slow("You look around in the darkness")
+    time.sleep(1)
+    print("\n")
+    print_slow(
+        "Silhouttes of the monsters you have slain laying on the ice cold floor")
+    time.sleep(1)
+    print("\n")
+    print_slow("You hear footseps approaching your room.")
+    time.sleep(1)
+    print("\n")
+    print_slow(
+        "You hear the wooden door creak as light floods your room from the outside")
+    time.sleep(1)
+    print("\n")
+    print_slow("A beam of light, bright as the sun flickers to life above you")
+    time.sleep(1)
+    print("\n")
+    print_slow(
+        "'George?! What are you doing up so late?' you hear a familiar voice say")
+    time.sleep(1)
+    print("\n")
+    print_slow(
+        "'Uhhh... Nothing mom. Just cleaning my room' you respond instinctively")
+    time.sleep(1)
+    print("\n")
+    print_slow(
+        "'Well you did not do a very good job then. Go to bed NOW!' she said strictly")
+    time.sleep(1)
+    print("\n")
+    print_slow("'Alright mom' you answer.")
+    time.sleep(1)
+    print("\n")
+    print_slow("As you stand up and look around, you see your monster stuffed animals laying on the ground. Next to you is a foam sword")
+    time.sleep(1)
+    print("\n")
+    print_slow("'I wish I could keep playing' I said to myself")
+    time.sleep(1)
+    print("\n")
+    print_slow("The light turns of as you curl up in your bed")
+    time.sleep(1)
+    print("\n")
+    print_slow("THE END")
+    print("\n")
+    print("\n")
+    time.sleep(6)
+    outro()
